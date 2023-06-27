58e7689b9c67a31b97d94fc9a28a587c542910e5
walkwithsanskar <walkwithsanskar1@gmail.com>
2023-06-28 05:01:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:["*"],
 		credentials:true,
 	})
 );
@@ -37,7 +37,7 @@ const server = app.listen(process.env.PORT , ()=>{
 const io = require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000"
+        origin:["*"]
     }
 });
 
